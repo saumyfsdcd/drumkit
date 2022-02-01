940a3700e62169b99b2d8bb1146bb4798bda0f68
sam <89059131+saumyfsdcd@users.noreply.github.com>
2022-02-01 17:33:47 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,64 @@
+document.addEventListener("keypress", function(event){
+    makesound(event.key);
+    anbutton(event.key);
+});
+function makesound(key){
+    switch(key){
+        case 'w': 
+            var audio=new Audio("crash.mp3");
+            audio.play();
+        break;
+
+        case 'a':
+            var audio=new Audio("kick-bass.mp3");
+            audio.play();
+        break;
+
+        case 's':
+            var audio=new Audio("snare.mp3");
+            audio.play();
+        break;
+
+        case 'd':
+            var audio=new Audio("tom-1.mp3");
+            audio.play();
+        break;
+
+        case 'j':
+            var audio=new Audio("tom-2.mp3");
+            audio.play();
+        break;
+
+        case 'k':
+            var audio=new Audio("tom-3.mp3");
+            audio.play();
+        break;
+
+        case 'l':
+            var audio=new Audio("tom-4.mp3");
+            audio.play();
+        break;
+    }
+
+}
+
+for(var i=0;i<8;i++){
+    document.querySelectorAll("button")[i].addEventListener("click", function(event){
+        makesound(this.innerHTML);
+        anbutton(this.innerHTML);
+    });
+}
+
+function anbutton(currentkey){
+    var active=document.querySelector("."+currentkey);
+    active.classList.add("pressed");
+
+    setTimeout(function(){
+        active.classList.remove("pressed");
+    }, 200)
+}
+
+
+
+
+
